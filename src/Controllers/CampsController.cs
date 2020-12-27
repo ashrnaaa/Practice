d64f40a2957984f82e582259bf2293c46a764e57
ashrnaaa <ashrnaaa@outlook.com>
2020-12-28 00:04:33 +0100

    initial commit

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections;
+using Microsoft.AspNetCore.Routing;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public CampsController(ICampRepository campRepository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            _campRepository = campRepository;
+            _mapper = mapper;
+            _linkGenerator = linkGenerator;
+        }
+        [HttpGet]
+        public async Task<ActionResult<CampsModel[]>> GetCamps(bool includeTalks = false)
+        {
+            try
+            {
+                var result = await _campRepository.GetAllCampsAsync(includeTalks);
+
+                return _mapper.Map<CampsModel[]>(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Something wrong on our end.");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampsModel>> GetCamp(string moniker, bool includeTalks = false)
+        {
+            try
+            {
+                var result = await _campRepository.GetCampAsync(moniker, includeTalks);
+
+                if (result == null)
+                    return NotFound();
+                return _mapper.Map<CampsModel>(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Something wrong on our end.");
+
+            }
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<CampsModel[]>> SearchByDate(DateTime theDate, bool includeTalks = false)
+        {
+            try
+            {
+                var results = await _campRepository.GetAllCampsByEventDate(theDate, includeTalks);
+
+                if (results == null)
+                    return NotFound();
+
+                return _mapper.Map<CampsModel[]>(results);
+            }
+            catch (Exception e)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Something wrong on our end.");
+
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CampsModel>> CreateCamp(CampsModel model)
+        {
+            try
+            {
+                var location = _linkGenerator.GetPathByAction($"GetCamp", $"Camps", new { moniker = model.Moniker });
+
+                if (string.IsNullOrWhiteSpace(location))
+                    return BadRequest("Could not use that moniker");
+                var camp = _mapper.Map<Camp>(model);
+                _campRepository.Add(camp);
+
+                if (await _campRepository.SaveChangesAsync())
+                {
+                    return Created(location, _mapper.Map<CampsModel>(camp));
+                }
+
+            }
+            catch (Exception e)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Something wrong on our end.");
+
+            }
+
+            return BadRequest("wrong input");
+        }
+    }
+}
---
+++ src/Data/CampProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Models;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Talk, TalksModel>();
+            this.CreateMap<Speaker, SpeakerModel>();
+            this.CreateMap<CampsModel, Camp>();
+            this.CreateMap<Camp, CampsModel>()
+                .ForMember(C => C.Venue, m => m.MapFrom(o => o.Location.VenueName));
+        }
+
+    }
+}
